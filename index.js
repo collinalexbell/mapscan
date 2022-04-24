bbecb08396f4f9f97fc32f261aef1aa925768ce1
Collin Bell <collinalexbell@gmail.com>
2022-04-24 12:56:55 -0400

    W is negative

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ let map;
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
       //40.7127° N, 74.0134° W
-    center: { lat: 40.7127, lng: 74.0134 },
+    center: { lat: 40.7127, lng: -74.0134 },
     zoom: 8,
   });
 }
