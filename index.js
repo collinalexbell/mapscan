e6815fb8edc0a1d0684d3d794e63434058532ea2
Collin Bell <collinalexbell@gmail.com>
2022-04-24 12:52:24 -0400

    make center be one world

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ let map;
 
 function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
+      //40.7127° N, 74.0134° W
+    center: { lat: 40.7127, lng: 74.0134 },
     zoom: 8,
   });
 }
